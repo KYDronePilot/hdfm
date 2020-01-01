12697a1d6deb96b7c5624b416150469880d7a790
KYDronePilot <kydronepilotdev@gmail.com>
2020-01-01 17:01:51 -0500

    Major refactoring of weather map management
     - Created an iHeartRadio config parser and OO-based config item
       representations.

--- src/us_map.py
+++ src/us_map.py
@@ -1,96 +1,253 @@
 import math
-import os
 import re
-from glob import glob
 from pathlib import Path
+from typing import ClassVar, List, Union, Dict, TextIO, Any, Tuple, Optional
 
 from PIL import Image
+from PIL.Image import Image as ImageType
 
-from . import DUMP, MAP_FILE, MAPS
+IHeartRadioConfigType = Dict[str, Union[str, List[str]]]
 
 
-# Obtains area maps in the US to overlay radar images on.
-class AreaMap:
+class IHeartRadioConfig:
+    """
+    iHeartRadio config manager.
+    """
+
+    @staticmethod
+    def strip_quotes(s: str) -> str:
+        """
+        Strip beginning/trailing quotes from string.
+
+        Args:
+            s: String to strip from
+
+        Returns:
+            Quote-stripped string
+        """
+        if s[0] == s[-1] == '"' or s[0] == s[-1] == "'":
+            return s[1:-1]
+
+    @staticmethod
+    def _parse_value(value: str) -> Union[List[str], str]:
+        """
+        Parse value part of config entry.
+
+        Args:
+            value: Entry value
+
+        Returns:
+            Parsed value
+        """
+        # Split into list if broken up by semicolons
+        if ';' in value:
+            return [IHeartRadioConfig.strip_quotes(part) for part in value.split(';')]
+        return IHeartRadioConfig.strip_quotes(value)
+
+    @staticmethod
+    def load(text: str) -> IHeartRadioConfigType:
+        """
+        Parse an iHeartRadio config file.
+
+        Args:
+            text: Text to parse
+
+        Returns:
+            Parsed data
+        """
+        result = {}
+        lines = text.splitlines()
+        for line in lines:
+            key_val_re = re.search(r'(\w+)=(.*?)$', line)
+            # Skip if line has no matching config entry
+            if key_val_re is None:
+                continue
+            key, val = key_val_re.groups()
+            result[key] = IHeartRadioConfig._parse_value(val)
+        return result
+
+
+class IHeartRadioConfigEntry:
+    """
+    Config entry in iHearRadio config.
+
+    Attributes:
+        key: Key for config entry
+        value: Value of config entry
+    """
+
+    key: ClassVar[str]
+    value: Any
+
+    def __init__(self, value: str):
+        self.value = self.parse(value)
+
+    def parse(self, value: str) -> Any:
+        """
+        Parse entry value.
+
+        Args:
+            value: Value to parse
+        """
+        return value
+
+
+class AreaId(IHeartRadioConfigEntry):
+    key = 'DWR_Area_ID'
+
+
+class Coordinates(IHeartRadioConfigEntry):
+    key = 'Coordinates'
+    value: Tuple[Tuple[float, float], Tuple[float, float]]
+
+    def parse(self, value: List[str]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+        if len(value) != 2:
+            raise Exception(f'Coordinates "{value}" in config are malformed.')
+        lat_top, lon_left = value[0][1:-1].split(',')
+        lat_bottom, lon_right = value[1][1:-1].split(',')
+        return (float(lat_top), float(lon_left)), (float(lat_bottom), float(lon_right))
+
+
+class MapManager:
+    """
+    Map to overlay the weather radar on.
+
+    Attributes:
+        lat_top: Latitude of top edge
+        lat_bottom: Latitude of bottom edge
+        lon_left: Longitude of left edge
+        lon_right: Longitude of right edge
+        area_id: Area ID of map
+        coordinates: Coordinates of map's edges
+        main_map: Large map of US from which smaller maps are taken
+        _map: Cached map instance for area specified by coordinates
+        cache_dir: Directory for caching map files
+    """
+
     # Max latitude of US map in a linear form.
-    LAT_MAX = 1.0799224683069641
+    LAT_MAX: ClassVar[float] = 1.0799224683069641
     # Reference latitude in linear form.
-    REF_LAT = 0.7380009964270406
+    REF_LAT: ClassVar[float] = 0.7380009964270406
 
-    def __init__(self):
-        # Coordinates of area.
-        self.lat1 = None
-        self.lon1 = None
-        self.lat2 = None
-        self.lon2 = None
-        # Area ID.
-        self.area_id = None
-        # The area map.
-        self.map = None
+    lat_top = float
+    lat_bottom = float
+    lon_left = float
+    lon_right = float
+    area_id = AreaId
+    coordinates: Coordinates
+    main_map: Path
+    _map: Optional[ImageType]
+    cache_dir: Path
 
-    # Get map configuration information.
-    def get_config(self):
+    def __init__(self, area_id: AreaId, coordinates: Coordinates, main_map: Path, cache_dir: Path):
+        self.area_id = area_id
+        self.coordinates = coordinates
+        self.main_map = main_map
+        self.lat_top = coordinates.value[0][0]
+        self.lat_bottom = coordinates.value[1][0]
+        self.lon_left = coordinates.value[0][1]
+        self.lon_right = coordinates.value[1][1]
+        self._map = None
+        self.cache_dir = cache_dir
+
+    @property
+    def map_cache_file(self) -> Path:
         """
-
-        :return: True if successful, False if not
+        Name of the map cache file.
         """
-        # Look for any radar map config files.
-        files = glob(os.path.join(DUMP, '*DWRI*'))
-        if not files:
-            return False
-        # Extract info from first file.
-        file = open(files[0], 'r')
-        for line in file:
-            if 'DWR_Area_ID' in line:
-                self.area_id = re.findall(r'\"(.+?)\"', line)[0]
-            elif 'Coordinates' in line:
-                coords = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-                self.lat1, self.lon1, self.lat2, self.lon2 = (float(x) for x in coords)
-        return True
+        return self.cache_dir / Path(f'map_{self.area_id.value}.png')
 
-    # Checks if a config file has been received.
-    def has_config(self):
-        return self.area_id is not None
+    @classmethod
+    def init_from_config(cls, fp: TextIO, main_map: Path, cache_dir: Path) -> 'Map':
+        """
+        Create instance from config.
 
-    # Render an area map from the US map.
-    def render(self):
-        # Open US map.
-        us_map = Image.open(MAP_FILE)
-        # Convert latitudes to a linear form.
-        self.lat1 = AreaMap.LAT_MAX - math.asinh(math.tan(math.radians(self.lat1)))
-        self.lat2 = AreaMap.LAT_MAX - math.asinh(math.tan(math.radians(self.lat2)))
-        # Calculate x-coords using a ratio of a known location on the map.
-        x1 = (self.lon1 + 130.781250) * 7162 / 39.34135
-        x2 = (self.lon2 + 130.781250) * 7162 / 39.34135
-        # Use another ratio of a known location to find the latitudes.
-        den = AreaMap.LAT_MAX - AreaMap.REF_LAT
-        y1 = self.lat1 * 3565 / den
-        y2 = self.lat2 * 3565 / den
+        Args:
+            fp: File handle
+            main_map: Path to main map
+            cache_dir: Directory where maps are cached
+
+        Returns:
+            New map instance
+        """
+        config_text = fp.read()
+        config = IHeartRadioConfig.load(config_text)
+        area_id = AreaId(config[AreaId.key])
+        coords = Coordinates(config[Coordinates.key])
+        return cls(area_id, coords, main_map, cache_dir)
+
+    def _load_main_map(self) -> ImageType:
+        """
+        Load main map to use for cropping.
+
+        Returns:
+            Loaded map
+        """
+        return Image.open(self.main_map)
+
+    def create_map(self) -> ImageType:
+        """
+        Create map for area specified by coordinates.
+
+        Returns:
+            Map for area
+        """
+        main_map = self._load_main_map()
+
+        # Make latitudes linear
+        def make_linear(val: float):
+            return MapManager.LAT_MAX - math.asinh(math.tan(math.radians(val)))
+
+        lin_lat_top = make_linear(self.lat_top)
+        lin_lat_bottom = make_linear(self.lat_bottom)
+        # Calculate x-coords using a ratio of a known location on the map
+        x1 = (self.lon_left + 130.781250) * 7162 / 39.34135
+        x2 = (self.lon_right + 130.781250) * 7162 / 39.34135
+        # Use another ratio of a known location to find the latitudes
+        den = MapManager.LAT_MAX - MapManager.REF_LAT
+        y1 = lin_lat_top * 3565 / den
+        y2 = lin_lat_bottom * 3565 / den
         # Crop the map.
-        cropped = us_map.crop((
+        cropped = main_map.crop((
             int(x1),
             int(y1),
             int(x2),
             int(y2)
         ))
-        # Resize to 900x900, convert to right format, and save.
-        self.map = cropped.resize((900, 900))
-        self.map = self.map.convert('RGBA')
-        name = '{0}.png'.format(self.area_id)
-        self.map.save(os.path.join(MAPS, name))
+        # Crop and resize the map
+        return cropped.resize((900, 900)).convert('RGBA')
 
-    # Get an area map for the predefined area.
-    def get_map(self):
-        # If already in memory, just return it.
-        if self.map is not None:
-            return self.map
-        # Check if an area map has already been rendered.
-        name = '{0}.png'.format(self.area_id)
-        file = Path(os.path.join(MAPS, name))
-        if file.is_file():
-            # Open and return image.
-            self.map = Image.open(file.absolute()).convert('RGBA')
-            return self.map
-        else:
-            # Else, render it and return.
-            self.render()
-            return self.map
+    @property
+    def map(self) -> ImageType:
+        """
+        Obtain map in the following order of attempts:
+            - Check cache attribute
+            - Check cache file
+            - Generate and cache new file
+
+        Returns:
+            Map specified by coordinates
+        """
+        if self._map is not None:
+            return self._map
+        if self.map_cache_file.exists():
+            map_image = Image.open(self.map_cache_file)
+            self.map = map_image
+            return map_image
+        map_image = self.create_map()
+        self.map = map_image
+        return map_image
+
+    @map.setter
+    def map(self, value: ImageType):
+        """
+        Write to cache file and attribute, overwriting if already exists.
+
+        Args:
+            value: Map image
+        """
+        self._map = value
+        # Delete cache if already exists
+        if self.map_cache_file.exists():
+            self.map_cache_file.unlink()
+        value.save(self.map_cache_file)
